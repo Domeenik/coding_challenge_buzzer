fad67000a3208f7a427da25124a75ee6014c2200
Domeenik <dominik@reifer.de>
2023-07-23 23:53:17 +0200

    Maybe i am done

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -32,6 +32,10 @@
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
 
+#define C6 1046.50
+#define D6 1174.66
+#define E6 1318.51
+
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
@@ -41,6 +45,7 @@
 
 /* Private variables ---------------------------------------------------------*/
 TIM_HandleTypeDef htim1;
+TIM_HandleTypeDef htim2;
 
 /* USER CODE BEGIN PV */
 
@@ -50,42 +55,52 @@ TIM_HandleTypeDef htim1;
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_TIM1_Init(void);
+static void MX_TIM2_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
-void buzzer_initiate() {
 
+void delay_us(uint16_t us){
+  // a functions that blocks until the timer 1 reaches the time given in us
+	__HAL_TIM_SET_COUNTER(&htim1,0);
+	while (__HAL_TIM_GET_COUNTER(&htim1) < (us));
 }
 
-void buzzer_deinitiate() {
-
+void set_buzzer_state(uint8_t active) {
+  if (active) {
+    //pin high -> active
+    GPIOC->BSRR = GPIO_BSRR_BS14;
+  } else {
+    // pin low -> inactive
+    GPIOC->BSRR = GPIO_BSRR_BR14;
+  }
 }
 
-void play_frequency(float frequency, uint16_t duration){
+void play_frequency(float frequency, uint16_t duration_ms){
+  // play the given frequency for the given amount of milliseconds
+  // timing for one half of the period in us
+	float period_us_float = 1000000.0 / (2.0 * frequency);
+	uint16_t period_us = (uint16_t)period_us_float;
 
+  // reset timer 2
+	__HAL_TIM_SET_COUNTER(&htim2,0);
+  // run "pwm" until timer reaches duration in milliseconds
+  while (__HAL_TIM_GET_COUNTER(&htim2) < (duration_ms)){
+    // high
+    GPIOA->BSRR = GPIO_BSRR_BS3;
+    delay_us(period_us);
+    // low
+    GPIOA->BSRR = GPIO_BSRR_BR3;
+    delay_us(period_us);
+  }
 }
 
 void play_test_tone() {
-  play_frequency(800.00, );
-}
-
-void play_note(char note, uint16_t duration) {
-  // play a note in the sixth octave
-  // get the corresponding frequency to the note
-  float frequency;
-  if (note == 'C') {
-    frequency = 1046.50;
-  } else if (note == 'D') {
-    frequency = 1174.66;
-  } else if (note == 'E') {
-    frequency = 1318.51;
-  } else {
-    frequency = 0;
-  }
-  play_frequency(frequency, duration);
+  // play the testing frequency for 1000ms
+  play_frequency(800.00, 1000);
 }
 
 /* USER CODE END 0 */
@@ -119,7 +134,9 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_TIM1_Init();
+  MX_TIM2_Init();
   /* USER CODE BEGIN 2 */
+  set_buzzer_state(1);
   play_test_tone();
   /* USER CODE END 2 */
 
@@ -190,14 +207,17 @@ static void MX_TIM1_Init(void)
 
   /* USER CODE END TIM1_Init 0 */
 
+  TIM_ClockConfigTypeDef sClockSourceConfig = {0};
   TIM_SlaveConfigTypeDef sSlaveConfig = {0};
   TIM_MasterConfigTypeDef sMasterConfig = {0};
+  TIM_OC_InitTypeDef sConfigOC = {0};
+  TIM_BreakDeadTimeConfigTypeDef sBreakDeadTimeConfig = {0};
 
   /* USER CODE BEGIN TIM1_Init 1 */
 
   /* USER CODE END TIM1_Init 1 */
   htim1.Instance = TIM1;
-  htim1.Init.Prescaler = 0;
+  htim1.Init.Prescaler = 400-1;
   htim1.Init.CounterMode = TIM_COUNTERMODE_UP;
   htim1.Init.Period = 65535;
   htim1.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
@@ -207,7 +227,16 @@ static void MX_TIM1_Init(void)
   {
     Error_Handler();
   }
-  sSlaveConfig.SlaveMode = TIM_SLAVEMODE_EXTERNAL1;
+  sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
+  if (HAL_TIM_ConfigClockSource(&htim1, &sClockSourceConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  if (HAL_TIM_PWM_Init(&htim1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sSlaveConfig.SlaveMode = TIM_SLAVEMODE_DISABLE;
   sSlaveConfig.InputTrigger = TIM_TS_ITR0;
   if (HAL_TIM_SlaveConfigSynchro(&htim1, &sSlaveConfig) != HAL_OK)
   {
@@ -220,9 +249,92 @@ static void MX_TIM1_Init(void)
   {
     Error_Handler();
   }
+  sConfigOC.OCMode = TIM_OCMODE_PWM1;
+  sConfigOC.Pulse = 0;
+  sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
+  sConfigOC.OCNPolarity = TIM_OCNPOLARITY_HIGH;
+  sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
+  sConfigOC.OCIdleState = TIM_OCIDLESTATE_RESET;
+  sConfigOC.OCNIdleState = TIM_OCNIDLESTATE_RESET;
+  if (HAL_TIM_PWM_ConfigChannel(&htim1, &sConfigOC, TIM_CHANNEL_1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sBreakDeadTimeConfig.OffStateRunMode = TIM_OSSR_DISABLE;
+  sBreakDeadTimeConfig.OffStateIDLEMode = TIM_OSSI_DISABLE;
+  sBreakDeadTimeConfig.LockLevel = TIM_LOCKLEVEL_OFF;
+  sBreakDeadTimeConfig.DeadTime = 0;
+  sBreakDeadTimeConfig.BreakState = TIM_BREAK_DISABLE;
+  sBreakDeadTimeConfig.BreakPolarity = TIM_BREAKPOLARITY_HIGH;
+  sBreakDeadTimeConfig.BreakFilter = 0;
+  sBreakDeadTimeConfig.Break2State = TIM_BREAK2_DISABLE;
+  sBreakDeadTimeConfig.Break2Polarity = TIM_BREAK2POLARITY_HIGH;
+  sBreakDeadTimeConfig.Break2Filter = 0;
+  sBreakDeadTimeConfig.AutomaticOutput = TIM_AUTOMATICOUTPUT_DISABLE;
+  if (HAL_TIMEx_ConfigBreakDeadTime(&htim1, &sBreakDeadTimeConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
   /* USER CODE BEGIN TIM1_Init 2 */
 
   /* USER CODE END TIM1_Init 2 */
+  HAL_TIM_MspPostInit(&htim1);
+
+}
+
+/**
+  * @brief TIM2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM2_Init(void)
+{
+
+  /* USER CODE BEGIN TIM2_Init 0 */
+
+  /* USER CODE END TIM2_Init 0 */
+
+  TIM_ClockConfigTypeDef sClockSourceConfig = {0};
+  TIM_SlaveConfigTypeDef sSlaveConfig = {0};
+  TIM_MasterConfigTypeDef sMasterConfig = {0};
+
+  /* USER CODE BEGIN TIM2_Init 1 */
+
+  /* USER CODE END TIM2_Init 1 */
+  htim2.Instance = TIM2;
+  htim2.Init.Prescaler = 4000-1;
+  htim2.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim2.Init.Period = 4294967295;
+  htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim2.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  if (HAL_TIM_Base_Init(&htim2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
+  if (HAL_TIM_ConfigClockSource(&htim2, &sClockSourceConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sSlaveConfig.SlaveMode = TIM_SLAVEMODE_DISABLE;
+  sSlaveConfig.InputTrigger = TIM_TS_ITR1;
+  if (HAL_TIM_SlaveConfigSynchro(&htim2, &sSlaveConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim2, &sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  if (HAL_TIMEx_RemapConfig(&htim2, TIM_TIM2_TI4_GPIO) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM2_Init 2 */
+
+  /* USER CODE END TIM2_Init 2 */
 
 }
 
@@ -240,9 +352,9 @@ static void MX_GPIO_Init(void)
   /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOE_CLK_ENABLE();
   __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
   __HAL_RCC_GPIOG_CLK_ENABLE();
   HAL_PWREx_EnableVddIO2();
-  __HAL_RCC_GPIOA_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIOE, GPIO_PIN_1, GPIO_PIN_RESET);
@@ -280,7 +392,7 @@ static void MX_GPIO_Init(void)
   /*Configure GPIO pin : PC5 */
   GPIO_InitStruct.Pin = GPIO_PIN_5;
   GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
   HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
 
   /*Configure GPIO pin : PA3 */
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # coding_challenge_buzzer
+
+Ich habe mich dafür entschieden den Ansatz zu implementieren, den ich in meinem letzten STM-Projekt verfolgt habe.
+Hierfür habe ich einen Timer (TIM1) so konfiguriert, dass dieser eine Periodendauer von einer Mikrosekunde hat.
+So kann ich die Periodendauer von der Noten-Frequenz berechnen und den Pin so lange mit dem doppelten Wert (rechteckige "Sinuswelle") ein und ausschalten.
+Dies wird so lange ausgeführt, bis ein zweiter Timer (TIM2), welcher die gewünschte Dauer in Millisekunden hochzählt über den Compare-Wert läuft.
+Ich habe des Weiteren angenommen, dass dem Buzzer die Spannung in eine Richtung reicht um einen Ton zu generieren.
+Ansonsten könnte die H-Brücke des Treibers verwendet werden um die Stromflussrichtung umzukehren und nicht nur den Stromfluss zu deaktivieren.
+
+Der Ansatz den ich gewählt habe ist wahrscheinlich nicht der effektivste.
+Es wäre sinnvoller einen Timer so zu konfigurieren, dass ein PWM-Signal generiert wird und die Frequenz von diesem variiert wird.
+Ich habe mich gegen diesen Ansatz entschieden, da dort der Timer in einer hohen Frequenz neu initiiert werden müsste.
+Es war für mich vor allem eine Herausforderung den Code nicht ausführen zu können.
+Normalerweise würde ich die einzelnen Komponenten anhand der Hardware mit meinem Oszi an meiner Seite testen.
